2cba2d02d2d354babba46e634521995ae7713ca7
Catalina Tarelli <catatarelli@hotmail.com>
2023-01-03 18:49:46 +0100

    Change ThemeProvider import from styled-components to mui

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "@mui/material";
 import mainTheme from "./styles/mainTheme";
-import GlobalStyle from "./styles/GlobalStyle";
 import "@fontsource/montserrat";
 import "@fontsource/open-sans";
 
@@ -14,7 +13,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={mainTheme}>
-      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
