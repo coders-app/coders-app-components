83b8aa38d23659ed57b812292a7a8d90a505e8df
Berto Forteza <bertoforteza87@gmail.com>
2023-01-23 15:45:51 +0100

    Apply new Theme interface to MainTheme and provide it to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "@mui/material";
-import mainTheme from "./styles/mainTheme";
 import "@fontsource/montserrat";
 import "@fontsource/open-sans";
+import { ThemeProvider } from "styled-components";
+import MainTheme from "./styles/MainTheme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
+    <ThemeProvider theme={MainTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
